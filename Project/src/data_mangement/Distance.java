d9bd97dddf21349cb67341d57923074bb2343f34
Artemiy Kokhanov <kokhanov.artemiy@gmail.com>
2018-03-14 20:18:11 -0400

    final dist for tn

--- Project/src/data_mangement/Distance.java
+++ Project/src/data_mangement/Distance.java
@@ -21,10 +21,10 @@ public class Distance {
 		String state = "MA";
 		
 		//this gets all the objects of a certain procedure number in a zip code range, and sets the distance from the user for all objects matching the described conditions.
-		PriorityQueue<Object> hospitals = (PriorityQueue<Object>) ReadExcel.combine.getHospitalsInRange(ReadExcel.combine.tree.keys(1040,1060),57, state);
-		System.out.println(hospitals.peek());
+		PriorityQueue<Object> hospitals = (PriorityQueue<Object>) ReadExcel.combine.getHospitalsInRange(ReadExcel.combine.tree.keys(1040,2060),57, state);
 		setDistances(hospitals, originAddress,originZip);
-		System.out.println(hospitals.peek());
+		while(hospitals.size() != 0)
+			System.out.println(hospitals.poll());
 
 		//System.out.println(ReadExcel.combine.getCities(ReadExcel.combine.tree.keys(1040,5000)));
 
