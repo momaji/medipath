ba802374ea8ac97d9ddb8c9b2705542c5e4ee87d
lynag <lynaghe@gmail.com>
2018-04-03 18:31:02 -0400

    new method

--- Project/src/data_mangement/ClientCode.java
+++ Project/src/data_mangement/ClientCode.java
@@ -10,25 +10,34 @@ public class ClientCode {
 		
 		String originAddress = "McMaster University";
 		String originZip = "L8S4L8";
-		
-		ArrayList<ProviderDataObject> locations= new ArrayList<ProviderDataObject>();
+		int[] prod = {57,178,101,189};
 		
 		//this gets all the objects of a certain procedure number in a zip code range, 
 		//and sets the distance from the user for all objects matching the described conditions.
-//<<<<<<< HEAD
+//<<<<<<< HEAD		
+	}
+	
+	public static void display(Edge[] path, ArrayList<ProviderDataObject> locations, int start) {
+		int trace = start;
+		for(int i = 0; i < locations.size()-1; i++) {
+			System.out.println(locations.get(trace) + "\n");
+			trace = path[trace].from();
+		}
+		System.out.println(locations.get(trace));
+	}
+	
+	public static void ShortPath(int[] prod) {
+		ArrayList<ProviderDataObject> locations= new ArrayList<ProviderDataObject>();
 		PriorityQueue<Object> hospitals = (PriorityQueue<Object>) ReadExcel.combine.getHospitalsInRange(ReadExcel.combine.tree.keys(1040,1600),57);
 		//Distance.setDistances(hospitals, originAddress,originZip);
-		
+
 		int count = 100;
 		while(hospitals.size() != 0){
 			((ProviderDataObject) hospitals.poll()).setDistance(count);
 			count += 10;
 		}
-		
-		
-		
-		hospitals = (PriorityQueue<Object>) ReadExcel.combine.getHospitalsInRange(ReadExcel.combine.tree.keys(1040,1600),57);
-		while(hospitals.size() != 0){
+		for(int i : prod) {
+			hospitals = (PriorityQueue<Object>) ReadExcel.combine.getHospitalsInRange(ReadExcel.combine.tree.keys(1040,1600),i);
 			locations.add((ProviderDataObject)hospitals.poll());
 		}
 		
@@ -41,23 +50,10 @@ public class ClientCode {
 				index = i;
 			}
 		}
-		//System.out.println(map);
+		
 		MinSpanningPath path = new MinSpanningPath(map, index);
-		
-		display(path.getPath(), locations, index);
-		
-		
-	}
-	
-	public static void display(Edge[] path, ArrayList<ProviderDataObject> locations, int start) {
-		int trace = start;
-		for(int i = 0; i < locations.size()-1; i++) {
-			System.out.println(locations.get(trace) + "\n");
-			trace = path[trace].from();
-		}
-		System.out.println(locations.get(trace));
+		display(path.getPath(), locations, index);	
 	}
 }
-
 		
 		
